185b6f996e6e8f10cb6a1fc0f92bd402bfa3812b
henrique-abacate <97639444+henrique-abacate@users.noreply.github.com>
2022-02-11 20:11:00 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 var trex, trex_running, edges;
 var groundImage;
 var nuvemMg;
-var text1=0;
+var score1=0;
 var cacto,cacto1, cacto2,cacto3,cacto4,cacto5,cacto6;
 var cactos;
 var nuvens;
+var END=0
+var PLAY=2
+var estadodojogo=PLAY;
+var morte, vivo, pulando;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
@@ -16,8 +20,10 @@ cacto3= loadImage ("obstacle3.png");
 cacto4= loadImage ("obstacle4.png");
 cacto5= loadImage ("obstacle5.png");
 cacto6= loadImage ("obstacle6.png");
+morte= loadSound("die.mp3");
+pulando= loadSound ("jump.mp3");
+vivo = loadSound("vivo1.mp3");
 }
-
 function setup(){
   createCanvas(600,200);
   
@@ -30,10 +36,13 @@ function setup(){
   trex.scale = 0.5;
   trex.x = 50
 
+trex.setCollider("circle",0,0,50);
+trex.debug=false;
+
  ground=createSprite(300,180,600,20);
 
 ground.addImage(groundImage);
-ground.velocityX=-6;
+ground.velocityX=-6-score1/100;
 invisibleGround=createSprite(300,190,600,10);
 invisibleGround.visible=false;
 cactos = new Group();
@@ -41,19 +50,29 @@ nuvens = new Group();
 }
 
 
+
 function draw(){
   //definir a cor do plano de fundo 
   background("white");
   textSize(20);
-  text("score"+text1,450,50);
+  text("score"+score1,450,50);
 
-  
+  if(estadodojogo===PLAY){
+score1=score1+ Math.round(getFrameRate()/150)
   //registrando a posição y do trex
-  console.log(trex.y)
-  
+  console.log("PLAY");
+if(score1%100===0&&score1>0){
+  vivo.play();
+}
+
+
+
+
   //pular quando tecla de espaço for pressionada
-  if(keyDown("space")&& trex.y>=161.5){
+  if(keyDown("space")&& trex.y>=160 ){
     trex.velocityY = -10;
+    pulando.play();
+  
   }
   
   trex.velocityY = trex.velocityY + 0.5;
@@ -61,15 +80,30 @@ function draw(){
     ground.x = ground.width /2;
   }
  //impedir que o trex caia
-  trex.collide(invisibleGround);
+
   nuvem();
   Pao();
+  if(cactos.isTouching(trex)){
+    estadodojogo=END;
+  morte.play();
+  }
+  }
+  else if(estadodojogo===END){
+    ground.velocityX=0
+    trex.velocityY=0
+    cactos.setVelocityXEach(0)
+    nuvens.setVelocityXEach(0)
+  cactos.setLifetimeEach(-1);
+  nuvens.setLifetimeEach(-1);
+
+  }
+  trex.collide(invisibleGround);
   drawSprites();
 }
 
 function nuvem (){
 if(frameCount%60==0){
-  var nuvem = createSprite (450,100,50,50);
+    var nuvem = createSprite (450,100,50,50);
   nuvem.addImage(nuvemMg);
   nuvem.y = Math.round(random(60,100))
   trex.depth=nuvem.depth+1
@@ -81,15 +115,16 @@ nuvens.add(nuvem);
 function Pao(){
   if(frameCount%60==0){
     var cacto = createSprite (450,175,50,50);
-  cacto.velocityX= -3;
+  cacto.velocityX= -3-score1/100;
   cacto.lifetime=150
+  cacto.scale=0.6;
   cactos.add(cacto);
 var numero= Math.round(random (1,6));
 switch(numero){
   case 1:cacto.addImage(cacto1);
   break;
   case 2:cacto.addImage(cacto2);
-  break;
+  break;1
   case 3:cacto.addImage(cacto3);
   break;
   case 4:cacto.addImage(cacto4);
